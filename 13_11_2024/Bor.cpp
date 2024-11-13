ada03dad75a284cf89ca68ee507385e671b18b5e
Senya <arsenijholkin02@gmail.com>
2024-11-13 15:56:24 +0300

    Bor done

--- 13_11_2024/Bor.cpp
+++ 13_11_2024/Bor.cpp
@@ -1,3 +1,32 @@
-//
-// Created by Senya on 13.11.2024.
-//
+#include "iostream"
+#include "map"
+#include "string"
+
+
+class Trie {
+private:
+    struct Node {
+        std::map<char, Node> next;
+        int value = 0;
+    } root;
+
+public:
+    int add(const std::string& str, int val) {
+        Node* v = &root;
+        for (const char el : str) v = &v->next[el];
+        v->value += val;
+        return v->value;
+    }
+};
+
+int main() {
+//    std::ios_base::sync_with_stdio(false);
+//    std::cin.tie(nullptr);
+
+    std::string str;
+    int value;
+    Trie trie;
+    while (std::cin >> str >> value) std::cout << trie.add(str, value) << '\n';
+
+    return 0;
+}
